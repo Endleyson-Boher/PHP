77b5fa59b9f0a13e60863dc74c71746aabb14f5a
Endleyson Boher <86278674+Endleyson-Boher@users.noreply.github.com>
2022-05-03 18:16:07 -0300

    Variaveis

---
+++ variaveis/exemplo-05.php
@@ -0,0 +1,21 @@
+<?php
+
+$nome = "Glaucio";
+
+function teste() {
+
+    global $nome;
+    echo $nome;
+
+}
+
+function teste2() {
+    $nome = " joão";
+    echo $nome." agora no teste 2";
+
+}
+
+teste();
+teste2();
+
+?>
